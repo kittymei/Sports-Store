b967e9f79dfee190bbfb47a7f4a526b14284563f
Kitty Mei <MEIK@my.erau.edu>
2019-09-17 11:38:19 -0700

    Add project files.

---
+++ Models/ViewModels/CartIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using SportsStore6.Models;
+
+namespace SportsStore6.Models.ViewModels
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using SportsStore6.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace SportsStore6
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDBContext>(options =>
+            options.UseSqlServer(
+                Configuration["Data:SportsStoreProducts:ConnectionString"]));
+            services.AddTransient<IProductRepository, EFProductRepository>();
+            //services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+                app.UseStaticFiles();
+                app.UseSession();
+                app.UseMvc(routes =>
+
+                {
+                    routes.MapRoute(
+                        name: null,
+                        template: "{category}/Page{productPage:int}",
+                        defaults: new {controller = "Product", action = "List"}
+                        );
+
+                    routes.MapRoute(
+                        name: null,
+                        template: "Page{productPage:int}",
+                        defaults: new
+                        {
+                            controller = "Product",
+                            action = "List",
+                            productPage = 1
+                        }
+                        );
+
+                    routes.MapRoute(
+                        name: null,
+                        template: "{category}",
+                        defaults: new
+                        {
+                            controller = "Product",
+                            actionk = "List",
+                            productPage = 1
+                        }
+                        );
+
+                    routes.MapRoute(
+                        name: null,
+                        template: "",
+                        defaults: new
+                        {
+                            controller = "Product", action = "List",
+                            productPage = 1
+                        }
+                        );
+
+                    routes.MapRoute(name: null, template: "{controller}/{action}/{id?}");
+
+
+                //routes.MapRoute(
+                //name: "pagination",
+                //template: "Products/Page{productPage}",
+                //defaults: new { Controller = "Product", action = "List" });
+
+                //routes.MapRoute(
+                //    name: "default",
+                //    template: "{controller=Product}/{action=List}/{id?}");
+            });
+
+                SeedData.EnsurePopulated(app);
+
+
+                app.Run(async (context) =>
+                {
+                    await context.Response.WriteAsync("It shouldn't happen at this level.");
+                });
+            }
+        }
+    }
+}
